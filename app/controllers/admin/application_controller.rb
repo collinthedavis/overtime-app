93b2edc35a2ae9ec5ea43f78c6bbbda03a0fa9a4
Collin <collinthedavis@gmail.com>
2016-10-28 14:31:03 -0600

    Added admin dashboard permission

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -4,18 +4,17 @@
 #
 # If you want to add pagination or other controller-level concerns,
 # you're free to overwrite the RESTful controller actions.
-module Admin
+
+  module Admin
   class ApplicationController < Administrate::ApplicationController
+    before_action :authenticate_user!
     before_filter :authenticate_admin
 
     def authenticate_admin
-      # TODO Add authentication logic here.
+      unless current_user.try(:type) == 'AdminUser'
+        flash[:alert] = "You are not authorized to access this page."
+        redirect_to(root_path)
+      end
     end
-
-    # Override this value to specify the number of elements to display at a time
-    # on index pages. Defaults to 20.
-    # def records_per_page
-    #   params[:per_page] || 20
-    # end
   end
 end
