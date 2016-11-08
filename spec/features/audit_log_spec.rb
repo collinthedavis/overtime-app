a4021a3dce9cdb5f25723f59900c43da8c4b541a
Collin <collinthedavis@gmail.com>
2016-11-08 10:02:21 -0700

    Styled pagination throughout application

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -1,28 +1,31 @@
 require 'rails_helper'
 
 describe 'AuditLog Feature' do
-  let(:audit_log) { FactoryGirl.create(:audit_log) }
-
-  describe 'index' do
-  	before do
-    	admin_user = FactoryGirl.create(:admin_user)
-    	login_as(admin_user, :scope => :user)
-    end
-
-    it 'renders audit log content' do
-  		FactoryGirl.create(:audit_log)
-  		visit audit_logs_path
-  		expect(page).to have_content(/SNOW/)
+	describe 'index' do
+		before do
+			admin_user = FactoryGirl.create(:admin_user)
+      login_as(admin_user, :scope => :user)
+      FactoryGirl.create(:audit_log)
 		end
 
-    it 'cannot be accessed by non admin users' do
-      logout(:user)
-      user = FactoryGirl.create(:user)
-      login_as(user, :scope => :user)
+		it 'has an index page that can be reached' do
+			visit audit_logs_path
+			expect(page.status_code).to eq(200)
+		end
 
-      visit audit_logs_path
+		it 'renders audit log content' do
+			visit audit_logs_path
+			expect(page).to have_content(/SNOW/)
+		end
 
-      expect(current_path).to eq(root_path)
-    end
-  end
+		it 'cannot be accessed by non admin users' do
+			logout(:user)
+			user = FactoryGirl.create(:user)
+			login_as(user, :scope => :user)
+
+			visit audit_logs_path
+
+			expect(current_path).to eq(root_path)
+		end
+	end
 end
